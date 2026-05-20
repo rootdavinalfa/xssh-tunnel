8c3df7bace5e1489ed7bd09aca2dcba6deb0d433
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2026-05-20 18:55:36 +0700

    chore: add component input

---
+++ src/lib/components/ui/input/input.svelte
@@ -0,0 +1,20 @@
+<script>
+  let { 
+    type = "text", 
+    value = $bindable(), 
+    placeholder = "", 
+    id = "", 
+    required = false,
+    ...rest 
+  } = $props();
+</script>
+
+<input
+  {type}
+  bind:value
+  {placeholder}
+  {id}
+  {required}
+  class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+  {...rest}
+/>
