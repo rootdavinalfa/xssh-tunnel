02eb92964a968acb64f1b53b34b655a8e7c6a2e5
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2026-05-20 21:27:28 +0700

    feat: bump to tailwind v4 and latest shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,32 +1,13 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { cubicOut } from "svelte/easing";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export function getDuration(node: Element, { duration = 200 }: { duration?: number } = {}) {
-	return {
-		duration
-	};
-}
-
-export function getCubicBezier(node: Element, { duration = 400, easing = cubicOut, start = 0, opacity = 0 }: { duration?: number; easing?: (t: number) => number; start?: number; opacity?: number } = {}) {
-	const style = getComputedStyle(node);
-	const target_opacity = +style.opacity || opacity;
-	const transform = style.transform === "none" ? "" : style.transform;
-
-	const sd = { opacity: target_opacity, transform };
-
-	return {
-		duration,
-		css: (t: number) => {
-			const eased = easing(t);
-			return `
-				opacity: ${eased * target_opacity};
-				transform: ${transform} scale(${1 - (1 - eased) * 0.05});
-			`;
-		}
-	};
-}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
   clearScreen: false,
   server: {
     port: 5173,
     strictPort: true,
     watch: {
-      ignored: ['**/src-tauri/**']
-    }
-  }
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 });
